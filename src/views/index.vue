45ff302925bdf93b76b6d0aa42cd890a3fea92bf
huabo <huabo1900@163.com>
2017-07-04 18:17:34 +0800

    heheh

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,23 +1,142 @@
 <style scoped>
-  .layout {
-    width: 100%;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-  }
-
-
+.layout {
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+}
+.layout-breadcrumb {
+    padding: 10px 15px 0;
+}
+.layout-content {
+    min-height: 600px;
+    margin: 15px;
+    overflow: hidden;
+    background: #fff;
+    border-radius: 4px;
+}
+.layout-content-main {
+    padding: 20px 10px;
+}
+.layout-copy {
+    text-align: center;
+    padding: 10px 0 20px;
+    color: #9ea7b4;
+}
+.layout-menu-left {
+    background: #464c5b;
+    /*min-width: 100px;*/
+}
+.layout-header {
+    height: 60px;
+    background: #fff;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, .1);
+}
+.layout-logo-left {
+    width: 90%;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    margin: 15px auto;
+    line-height: 30px;
+    color: white;
+    text-align: center;
+    vertical-align: middle;
+    overflow: hidden;
+}
+.layout-ceiling-main a {
+    color: #9ba7b5;
+}
+.layout-hide-text .layout-text {
+    display: none;
+}
+.ivu-col {
+    transition: width .2s ease-in-out;
+}
 </style>
-<template>
-    <div class="layout">
-      <Row type="flex" style="height:100%;">
-        这里是首页
-      </Row>
-    </div>
-</template>
-<script>
-    export default {
 
-    }
+<template>
+
+<div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
+    <Row type="flex">
+        <i-col :span="spanLeft" class="layout-menu-left">
+          <div class="layout-logo-left">后台管理</div>
+          <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']" @on-select="routeTo">
+                  <Submenu name="1">
+                      <template slot="title">
+                          <Icon type="ios-navigate"></Icon>
+                          导航一
+                      </template>
+                      <Menu-item name="1-1">选项 1</Menu-item>
+                      <Menu-item name="1-2">选项 2</Menu-item>
+                      <Menu-item name="1-3">选项 3</Menu-item>
+                  </Submenu>
+                  <Submenu name="2">
+                      <template slot="title">
+                          <Icon type="ios-keypad"></Icon>
+                          导航二
+                      </template>
+                      <Menu-item name="2-1">选项 1</Menu-item>
+                      <Menu-item name="2-2">选项 2</Menu-item>
+                  </Submenu>
+                  <Submenu name="3">
+                      <template slot="title">
+                          <Icon type="ios-analytics"></Icon>
+                          导航三
+                      </template>
+                      <Menu-item name="3-1">选项 1</Menu-item>
+                      <Menu-item name="3-2">选项 2</Menu-item>
+                  </Submenu>
+              </Menu>
+        </i-col>
+        <i-col :span="spanRight">
+            <div class="layout-header">
+            </div>
+            <div class="layout-breadcrumb">
+                <Breadcrumb>
+                    <Breadcrumb-item>首页</Breadcrumb-item>
+                    <Breadcrumb-item>页面</Breadcrumb-item>
+                    <Breadcrumb-item>{{this.$route.path.replace('/','')}}</Breadcrumb-item>
+                </Breadcrumb>
+            </div>
+            <div class="layout-content">
+                <div class="layout-content-main">
+                    <transition mode="out-in">
+                        <router-view></router-view>
+                    </transition>
+                </div>
+            </div>
+            <div class="layout-copy">
+                2011-2016 &copy; NSLab
+            </div>
+        </i-col>
+    </Row>
+</div>
+
+</template>
+
+<script>
+export default {
+    data() {
+            return {
+                spanLeft: 5,
+                spanRight: 19,
+                page: ['about','form','table','markdown-viewer', 'markdown-editor-1', 'markdown-editor-2','rtf','upload','echarts']
+            }
+        },
+        computed: {
+            iconSize() {
+                return this.spanLeft === 5 ? 14 : 24;
+            },
+            setActive() {
+              return this.$route.path.replace('/','');
+            }
+        },
+        methods: {
+                routeTo(e) {
+                    console.log(e);
+                }
+        }
+}
 </script>
